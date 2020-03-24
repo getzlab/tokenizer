a4bc3059cd76727eae8e4b4c0d0dd3463ae4f2ea
Julian Hess <jhess@broadinstitute.org>
2020-03-24 18:10:43 -0400

    Speed up token criteria check

--- get_all_calls.cpp
+++ get_all_calls.cpp
@@ -16,35 +16,44 @@ void gac_walker::walk_apply(const SeqLib::BamRecord& record) {
 	 cur_read.ChrID() == curchr) ||
 	 cur_read.ChrID() > curchr) {
 
-      // flush to disk
-      
-      uint32_t& alt_count = pos_cache.at(curpos).alt_count;
-      uint32_t& ref_count = pos_cache.at(curpos).ref_count;
-      
-      float alt_frac = ((float) alt_count)/((float) alt_count + (float) ref_count) > 0.001
+      // flush to disk 
+      float alt_frac;
+      uint32_t alt_count;
+      uint32_t ref_count;
+
+      if(pos_cache.contains(curpos)) {
+	 alt_count = pos_cache.at(curpos).alt_count;
+	 ref_count = pos_cache.at(curpos).ref_count;
+	 alt_frac = ((float) alt_count)/((float) alt_count + (float) ref_count);
+      } else {
+	 alt_count = 0;
+	 ref_count = 0;
+	 alt_frac = 0.0;
+      } 
+
+      /*
+      - : total counts < 8 (i.e. insufficient coverage)
+      + : total counts >= 8, notwithstanding higher token criteria
+      1 : alt count >= 1 and alt fraction >= 0.1%
+      2 : alt count >= 2 and alt fraction >= 0.3%
+      3 : alt count >= 3 and alt fraction >= 1%
+      4 : alt count >= 3 and alt fraction >= 3%
+      5 : alt count >= 3 and alt fraction >= 20%
+      6 : alt count >= 10 and alt fraction >= 20%
+      */
 
       uint8_t tok_i = 0;
-      if(alt_count + ref_count >= 8) tok_i++; // +
-      if(alt_count >= 1 && alt_frac >= 0.001) tok_i++; // 1
-      if(alt_count >= 2 && alt_frac >= 0.003) tok_i++; // 2
-      if(alt_count >= 3 && alt_frac >= 0.01) tok_i++; // 3
-      if(alt_count >= 3 && alt_frac >= 0.03) tok_i++; // 4
-      if(alt_count >= 3 && alt_frac >= 0.20) tok_i++; // 5
-      if(alt_count >= 10 && alt_frac >= 0.20) tok_i++; // 5
+      if(alt_count + ref_count >= 8) { tok_i++; // +
+      if(alt_count >= 1 && alt_frac >= 0.001) { tok_i++; // 1
+      if(alt_count >= 2 && alt_frac >= 0.003) { tok_i++; // 2
+      if(alt_count >= 3 && alt_frac >= 0.01) { tok_i++; // 3
+      if(alt_count >= 3 && alt_frac >= 0.03) { tok_i++; // 4
+      if(alt_count >= 3 && alt_frac >= 0.20) { tok_i++; // 5
+      if(alt_count >= 10 && alt_frac >= 0.20) { tok_i++; // 6
+      } } } } } } }
 
       //"-+123456"[tok_i]
       fwrite("-+123456" + tok_i, 1, 1, outfile);
-
-/*
-- : total counts < 8 (i.e. insufficient coverage)
-+ : total counts >= 8, notwithstanding higher token criteria
-1 : alt count >= 1 and alt fraction >= 0.1%
-2 : alt count >= 2 and alt fraction >= 0.3%
-3 : alt count >= 3 and alt fraction >= 1%
-4 : alt count >= 3 and alt fraction >= 3%
-5 : alt count >= 3 and alt fraction >= 20%
-6 : alt count >= 10 and alt fraction >= 20%
-*/
 
       pos_cache.erase(curpos);
 
