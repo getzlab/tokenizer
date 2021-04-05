04e74abd91fc1a6f038c14a64d4b18bf27bae98d
Julian Hess <jhess@broadinstitute.org>
2021-04-05 12:18:34 -0400

    Initial commit of wolF scripts

---
+++ wolF/tasks.py
@@ -0,0 +1,8 @@
+from wolf import Task
+
+class tokenizer(Task):
+    name = "tokenizer"
+    inputs = { "bam", "ref_fa", "prefix" }
+    script = "/app/tokenizer -b ${bam} -r ${ref_fa} -o ${prefix}.tok"
+    output_patterns = { "tokens" : "*.tok" }
+    docker = "gcr.io/broad-getzlab-workflows/tokenizer:v28"
