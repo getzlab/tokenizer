0ce1eb7b4c735a8bb0b4fb71f05accd8323ae4e4
Julian Hess <jhess@broadinstitute.org>
2021-04-13 16:02:50 +0000

    Bump memory

--- wolF/tasks.py
+++ wolF/tasks.py
@@ -6,3 +6,4 @@ class tokenizer(Task):
     script = "/app/tokenizer -b ${bam} -r ${ref_fa} -o ${prefix}.tok"
     output_patterns = { "tokens" : "*.tok" }
     docker = "gcr.io/broad-getzlab-workflows/tokenizer:v28"
+    resources = { "mem" : "2GB" }
